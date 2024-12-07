2875ab1754e9761c97531ff677bc57983f6623d2
Yap <123754664+shengpeiyap@users.noreply.github.com>
2024-12-07 19:57:34 +0800

    Delete CW WD directory

--- CW WD/navigation.js
+++
@@ -1,5 +0,0 @@
-// Toggle the menu visibility when clicking the hamburger icon
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-});
